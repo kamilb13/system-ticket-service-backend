cb72523b851ecdf165d4e00f5c45df297177fa16
kamilb13 <kamil.brzoza13@gmail.com>
2026-03-17 20:10:29 +0100

    first commit

---
+++ src/main/java/pl/projekt/psk/systemticketservicebackend/SystemTicketServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.projekt.psk.systemticketservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemTicketServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemTicketServiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/projekt/psk/systemticketservicebackend/SystemTicketServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projekt.psk.systemticketservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemTicketServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
