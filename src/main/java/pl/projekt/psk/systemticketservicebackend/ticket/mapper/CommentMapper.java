9b9f2a4dbee5f3d8a83a715728e9fb0e7a832b24
Mikołaj Pacierz <mikolajpacierz18@gmail.com>
2026-03-23 13:12:38 +0100

    fix: fixed adding new ticket

--- src/main/java/pl/projekt/psk/systemticketservicebackend/ticket/mapper/CommentMapper.java
+++ src/main/java/pl/projekt/psk/systemticketservicebackend/ticket/mapper/CommentMapper.java
@@ -3,11 +3,16 @@ package pl.projekt.psk.systemticketservicebackend.ticket.mapper;
 import pl.projekt.psk.systemticketservicebackend.ticket.dto.CommentDto;
 import pl.projekt.psk.systemticketservicebackend.ticket.model.Comment;
 
+import java.util.Collections;
 import java.util.List;
 
 public class CommentMapper {
 
     public static List<CommentDto> toCommentDto(List<Comment> allByTicketId) {
+        if (allByTicketId == null) {
+            return Collections.emptyList();
+        }
+
         return allByTicketId.stream()
                 .map(comment -> new CommentDto(comment.getId(), comment.getCommentContent(), comment.getUsername()))
                 .toList();
